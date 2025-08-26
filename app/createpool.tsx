9122d27c215c235264ba21d522d5bf3a51259c09
JoJo <70014631+Emekabest@users.noreply.github.com>
2025-08-26 09:51:27 +0100

    finished reconstructing createPollScreen

--- app/createpool.tsx
+++ app/createpool.tsx
@@ -3,7 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as ImagePicker from "expo-image-picker";
 import { useFocusEffect } from "expo-router";
 import React, { useCallback, useEffect, useState } from "react";
-import { FlatList, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Dimensions, FlatList, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import CreatePollController from "./Controller/CreatePollController";
 import Header from "./header";
 import Loader from "./loader";
@@ -19,7 +19,8 @@ let setIsLoader_global:any;
 const CreatePool = ()=>{
 
 
-
+    const { height } = Dimensions.get("window");
+    
 
     const loaderStore = useSharedStore((state) => state.value); // ✅ directly call the hook
     const setLoaderStore = useSharedStore((state) => state.setValue); // ✅ directly call the hook
@@ -200,7 +201,7 @@ const CreatePool = ()=>{
     
 
     return(
-        <View>
+        <View className="h-[100%]" style={{backgroundColor:AppDetails.color.backgroundColor}}>
 
             {
                 isLoader ?
@@ -212,101 +213,100 @@ const CreatePool = ()=>{
                 ""
             }
         
-            
-            <Header />
-            <View className="h-[92%] mt-20" style={{backgroundColor:AppDetails.color.backgroundColor}}>
-                <View className="h-[40%] pt-3 px-4" >
+            <View>
+                <Header />
+                <View className="mt-20" style={{backgroundColor:AppDetails.color.backgroundColor, height:height - AppDetails.header.height, marginTop:AppDetails.header.height}}>
+                    <View className="h-[40%] px-4 mb-4" >
 
-                    <Text className="font-nunito-bold text-center text-lg color-[#333]">Enter Canditate Details</Text>
 
-                    <View className="pt-5">
+                        <View className="">
+                            <TextInput className="text-xl text-center rounded-full font-nunito-bold bg-gray-100"  placeholderTextColor="gray" value={pollname} onChangeText={setPollName}  placeholder="Enter poll name" />
 
-                        <TextInput className="h-12 rounded-full px-5 text-lg mb-2" placeholderTextColor="gray" value={candidateFirstName} onChangeText={setCandidateFirstName} placeholder="firstname" />
+                            <TextInput className="h-12 rounded-full px-5 text-lg mb-2" placeholderTextColor="gray" value={candidateFirstName} onChangeText={setCandidateFirstName} placeholder="Enter candidate firstname" />
 
-                        <TextInput className="h-12 rounded-full px-5 text-lg" placeholderTextColor="gray" value={candidateLastName} onChangeText={setCandidateLastName} placeholder="lastname" />
+                            <TextInput className="h-12 rounded-full px-5 text-lg mb-3" placeholderTextColor="gray" value={candidateLastName} onChangeText={setCandidateLastName} placeholder="Enter candidate lastname" />
 
-                        <View className="h-28 flex-row">
+                            <View className="h-24 flex-row">
 
-                            <View className="h-[100%] w-[40%] rounded-lg bg-gray-200 items-center justify-center overflow-hidden">
-                                <Image
-                                    source={{ uri: candidateImage }}
-                                    style={{ width: "100%", height: "100%"}}
-                                />
+                                <View className="h-[100%] w-[30%] rounded-lg bg-gray-200 items-center justify-center overflow-hidden">
+                                    <Image
+                                        source={{ uri: candidateImage }}
+                                        style={{ width: "100%", height: "100%"}}
+                                    />
+                                </View>
+                                
+                                <View className=" w-20 items-center justify-center">
+                                    <TouchableOpacity className="w-10 h-10 items-center rounded-full justify-center" onPress={handleSelectImage} activeOpacity={1} style={{backgroundColor: AppDetails.color.iconColors}}>
+                                        <FontAwesome name="plus" size={15} color="#fff"  className="" />
+                                        
+                                    </TouchableOpacity>
+                                </View>
+
                             </View>
+
                             
-                            <View className=" w-20 items-center justify-center">
-                                <TouchableOpacity className="w-10 h-10 items-center rounded-full justify-center" onPress={handleSelectImage} activeOpacity={1} style={{backgroundColor: AppDetails.color.iconColors}}>
-                                    <FontAwesome name="plus" size={15} color="#fff"  className="" />
-                                    
+                            <View className="w-[100%] items-center justify-center">
+
+                                <Text className="pb-2 text-sm color-red-600">{formFeedbackMsg}</Text>
+
+                                <TouchableOpacity  activeOpacity={1} className="h-12 w-[100%] rounded-full bg-[#C4A484] items-center justify-center" onPress={()=> handleSubmitCandidateData()}>
+                                    <Text className="font-nunito-bold text-xl color-[#333]">Submit</Text>
                                 </TouchableOpacity>
-                                {/* <Text>Select Image</Text> */}
                             </View>
-
-                        </View>
-
-                        
-                        <View className="w-[100%] items-center justify-center">
-
-                            <Text className="pb-2 text-sm color-red-600">{formFeedbackMsg}</Text>
-
-                            <TouchableOpacity  activeOpacity={1} className="h-12 w-[100%] rounded-full bg-[#C4A484] items-center justify-center" onPress={()=> handleSubmitCandidateData()}>
-                                <Text className="font-nunito-bold text-xl color-[#333]">Submit</Text>
-                            </TouchableOpacity>
                         </View>
                     </View>
-                </View>
 
-                
+                    
 
-                <View className="h-[60%] px-4">
-                    <TextInput className="h-[10%] text-lg text-center" placeholderTextColor="gray" value={pollname} onChangeText={setPollName}  placeholder="Enter name of poll" />
+                    <View className="h-[60%] px-4">
 
 
-                        <FlatList
-                            data={candidateList}
-                            keyExtractor={(candidateList) => candidateList.id}
-                            renderItem={({ item }) => (
+                            <FlatList
+                                data={candidateList}
+                                keyExtractor={(candidateList) => candidateList.id}
+                                renderItem={({ item }) => (
 
-                                <View className="h-20 w-[100%] mb-3 flex-row">
-                                    <View className="w-[70%] h-[100%] flex-row">
-                                        <View className="h-[100%] w-[30%]">
-                                                {
-                                                    item.candidateImage ?
+                                    <View className="h-20 w-[100%] mb-3 flex-row">
+                                        <View className="w-[70%] h-[100%] flex-row">
+                                            <View className="h-[100%] w-[30%]">
+                                                    {
+                                                        item.candidateImage ?
 
-                                                    <Image
-                                                        source={{ uri: item.candidateImage }}
-                                                        style={{ width: "100%", height: "100%"}}
-                                                    />
-                                                    :
-                                                    <View className="h-[100%] w-[100%] items-center justify-center">
-                                                        <FontAwesome name="user-circle" size={50} color="#aaa"  className="" />
-                                                    </View>
+                                                        <Image
+                                                            source={{ uri: item.candidateImage }}
+                                                            style={{ width: "100%", height: "100%"}}
+                                                        />
+                                                        :
+                                                        <View className="h-[100%] w-[100%] items-center justify-center">
+                                                            <FontAwesome name="user-circle" size={50} color="#aaa"  className="" />
+                                                        </View>
 
-                                                }
+                                                    }
+
+                                            </View>
+
+                                            <View className="h-[100%] w-[70%] justify-center ml-3">
+                                                    <Text className="font-nunito-bold text-lg color-[#333]">{item.candidateFirstName} {item.candidateLastName}</Text>
+                                            </View>
+                                            
 
                                         </View>
-
-                                          <View className="h-[100%] w-[70%] justify-center ml-3">
-                                                <Text className="font-nunito-bold text-lg color-[#333]">{item.candidateFirstName} {item.candidateLastName}</Text>
+                                        <View className="w-[30%] h-[100%] justify-center items-end ">
+                                            <TouchableOpacity onPress={()=> handleDeleteCandidateList(item.id)}>
+                                                <FontAwesome name="trash" size={25} color="#C4A484"/>
+                                            </TouchableOpacity>
                                         </View>
-                                        
-
                                     </View>
-                                    <View className="w-[30%] h-[100%] justify-center items-end ">
-                                        <TouchableOpacity onPress={()=> handleDeleteCandidateList(item.id)}>
-                                            <FontAwesome name="trash" size={25} color="#C4A484"/>
-                                        </TouchableOpacity>
-                                    </View>
-                                </View>
-                            )}
-                            showsVerticalScrollIndicator={false}
-                        />
+                                )}
+                                showsVerticalScrollIndicator={false}
+                            />
 
 
-                    <TouchableOpacity className="h-16 w-[100%] rounded-full bg-[#C4A484] items-center justify-center mb-5" onPress={handleCreatePoll}>
-                        <Text className="font-nunito-bold color-[#333] text-2xl">Create</Text>
-                    </TouchableOpacity>
+                        <TouchableOpacity className="h-16 w-[100%] rounded-full bg-[#C4A484] items-center justify-center" onPress={handleCreatePoll}>
+                            <Text className="font-nunito-bold color-[#333] text-2xl">Create</Text>
+                        </TouchableOpacity>
 
+                    </View>
                 </View>
             </View>
         </View>
